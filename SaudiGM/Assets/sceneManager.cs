f7523287968386c61bb10158a9ed790169898cd5
KhaledKhalil420 <108684316+KhaledKhalil420@users.noreply.github.com>
2022-07-06 08:15:03 +0300

    SceneManager

--- SaudiGM/Assets/sceneManager.cs
+++ SaudiGM/Assets/sceneManager.cs
@@ -1,18 +1,38 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.Collections;
 
 public class sceneManager : MonoBehaviour
 {
     [Header("Put DeathRoom Name")]
     public string DeathRoom1, DeathRoom2, DeathRoom3;
 
+    public Animator Anim;
+
     public void LoadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(NextLevelT());
     }
 
     public void LoadRandomDeathRoom()
     {
+        StartCoroutine(ChoseRandomLevel());
+    }
+
+    void Update()
+    {
+        //Remove After Realese
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    IEnumerator ChoseRandomLevel()
+    {
+        yield return new WaitForSeconds(0.1f);
+        //Make a Trigger and Call It Fade
+        Anim.SetTrigger("Fade");
+        yield return new WaitForSeconds(1);
         int RandomNumber = Random.Range(1, 4);
         if(RandomNumber == 1)
         SceneManager.LoadScene(DeathRoom1);
@@ -23,12 +43,12 @@ public class sceneManager : MonoBehaviour
         if(RandomNumber == 3)
         SceneManager.LoadScene(DeathRoom3);
     }
-
-    void Update()
+    IEnumerator NextLevelT()
     {
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+        yield return new WaitForSeconds(0.1f);
+        //Make a Trigger and Call It Fade
+        Anim.SetTrigger("Fade");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
